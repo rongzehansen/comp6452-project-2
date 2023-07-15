e078514eb163380edf1520a7b71be8dcea94def1
Rtyyio <47835523+Rtyyio@users.noreply.github.com>
2023-07-15 23:11:25 +1000

    Home page
    Add home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,111 +1,18 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { ethers } from 'ethers';
-import './App.css';
-import contract from './contracts/contract.json'
 
-const contractAddress = "0xA2488deA449955e35789eC6C13e3cd19f7a0A98c";
-const abi = contract;
+import './App.css';
+import {Home} from './component/home';
+
+
 
 
 function App() {
-    const checkWalletIsConnected = async () => {     
-        const { ethereum } = window;
-        if(!ethereum){
-            console.log("Please install the Metamask Wallet!");
-            return;
-        }
-        else{
-            console.log("Wallet exists. Ready to go!");
-        }
-
-        const accounts  = await ethereum.request({ method: 'eth_accounts' });
-
-        if(accounts.length !== 0){
-            const account = accounts[0];
-            console.log("Authorized Account Found! Address is：", account);
-            setCurrentAccount(account);
-        }
-        else{
-            console.log("No Authorized Account Found.");
-        }
-        
-    }
-
-    const [currentAccount, setCurrentAccount] = useState(null);
-
-    const connectWalletHandler = async () => { 
-        const { ethereum } = window;
-        if(!ethereum){
-            alert("Please install the Metamask Wallet!");
-        }
-
-        try{
-            const accounts = await ethereum.request({method: 'eth_requestAccounts'});
-            console.log("Account Found! Address is：", accounts[0]);
-            setCurrentAccount(accounts[0]);
-        }
-        catch(err){
-            console.log(err);
-        }
-    }
-  
-    const reportHandler = async () => { 
-        try{
-            const { ethereum } = window;
-
-            if(ethereum){
-                const provider = new ethers.BrowserProvider(ethereum);
-                const signer = await provider.getSigner();
-                const tradeContract = new ethers.Contract(contractAddress, abi, signer);
-                
-                console.log("Create account");
-                let tradeTxn = await tradeContract.createAccount("0xC0436A7e7Eba86F044a0257ac5AE3a4c102Ca591","ABC");
-                console.log(tradeTxn);
-
-                console.log("Mining, plz wait");
-                //await tradeTxn.wait();
-
-                console.log('Mined, see transaction: https://sepolia.etherscan.io/tx/${tradeTxn.hash}');
-            }
-            else{
-                console.log("Ethereum object does not exist");
-            }
-
-        } 
-        catch(err){
-            console.log(err);
-        }
-    }
-  
-    const connectWalletButton = () => {
-      return (
-        <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
-          Connect Wallet
-        </button>
-      )
-    }
-  
-    const reportButton = () => {
-      return (
-        <button onClick={reportHandler} className='cta-button mint-nft-button'>
-          Generate Report
-        </button>
-      )
-    }
-  
-    useEffect(() => {
-      checkWalletIsConnected();
-    }, [])
-  
-    return (
-      <div className='main-app'>
-        <h1>6452 Assignment 2</h1>
-        <div>
-          {currentAccount ? reportButton() : connectWalletButton()}
-        </div>
+    return(
+      <div>
+        <Home></Home>
       </div>
-    )
-  }
+
+    )    
+
+}
   
   export default App;
---
+++ frontend/src/component/home.js
@@ -0,0 +1,139 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import contract from '../contracts/contract.json'
+const contractAddress = "0xE09490B23A44070cA06706b92a86DF398B159358";
+const abi = contract;
+export function Home(){
+    const checkWalletIsConnected = async () => {     
+        const { ethereum } = window;
+        if(!ethereum){
+            console.log("Please install the Metamask Wallet!");
+            return;
+        }
+        else{
+            console.log("Wallet exists. Ready to go!");
+        }
+
+        const accounts  = await ethereum.request({ method: 'eth_accounts' });
+
+        if(accounts.length !== 0){
+            const account = accounts[0];
+            console.log("Authorized Account Found! Address is：", account);
+            setCurrentAccount(account);
+        }
+        else{
+            console.log("No Authorized Account Found.");
+        }
+        
+    }
+
+    const checkUserAccount = async()=>{
+        try{
+            const { ethereum } = window;
+
+            if(ethereum){
+                const provider = new ethers.BrowserProvider(ethereum);
+                const signer = await provider.getSigner();
+                if(currentAccount==null) return;
+                const tradeContract = new ethers.Contract(contractAddress, abi, signer);
+                
+                console.log("Create account");
+                let tradeTxn = await tradeContract.getUserInfo(currentAccount);
+                if(tradeTxn!="" || tradeTxn!=null) setName(tradeTxn);
+            }
+            else{
+                console.log("Ethereum object does not exist");
+            }
+
+        } 
+        catch(err){
+            console.log(err);
+        }
+
+    }
+
+    const [currentAccount, setCurrentAccount] = useState(null);
+    const [name, setName] = useState('');
+
+    const connectWalletHandler = async () => { 
+        const { ethereum } = window;
+        if(!ethereum){
+            alert("Please install the Metamask Wallet!");
+        }
+
+        try{
+            const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+            console.log("Account Found! Address is：", accounts[0]);
+            setCurrentAccount(accounts[0]);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+    
+
+    const connectWalletButton = () => {
+    return (
+        <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+        </button>
+    )
+    }
+
+    function createSection() {
+        const handleInputChange = (event) => {
+            setName(event.target.value);
+          };
+
+        const reportHandler =async () => { 
+            alert("yessssss");
+        try{
+            const { ethereum } = window;
+
+            if(ethereum){
+                const provider = new ethers.BrowserProvider(ethereum);
+                const signer = await provider.getSigner();
+                const tradeContract = new ethers.Contract(contractAddress, abi, signer);
+                
+                console.log("Create account");
+                let tradeTxn = await tradeContract.createAccount(currentAccount,name);
+                console.log(tradeTxn);
+            }
+            else{
+                console.log("Ethereum object does not exist");
+            }
+
+        } 
+        catch(err){
+            console.log(err);
+        }
+    }
+        
+    return (
+        <div>
+            <input onChange={handleInputChange} />
+            <button onClick={reportHandler} className='cta-button mint-nft-button'>
+            Create Account
+            </button>
+        </div>
+    )
+    }
+
+    useEffect(() => {
+        checkWalletIsConnected();
+        if(currentAccount)
+        checkUserAccount();
+    }, [currentAccount,name])
+
+    return (
+    <div className='main-app'>
+        <h1>6452 Assignment 2</h1>
+        {name?<div>Welcome {name}</div> : <div></div> }
+        <div>
+        {currentAccount ? createSection() : connectWalletButton()}
+        </div>
+    </div>
+    )
+}
