153b75a7a96445f9e09f63c3129eb63d59654968
Rongze Han <rongze.hansen@gmail.com>
2023-07-17 20:56:38 +1000

    Update Interface.sol

--- contracts/Interface.sol
+++ contracts/Interface.sol
@@ -1,4 +1,6 @@
 /// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
 interface IAccountManager {
     function getBalance(address userAddress) external returns (uint);
     function releaseFunds(address senderAddress, address targetAddress, uint amount) external;
