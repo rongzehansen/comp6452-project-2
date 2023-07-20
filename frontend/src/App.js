6244d8d02b6ad12667dac6930bd1c86cf433b92c
Rtyyio <47835523+Rtyyio@users.noreply.github.com>
2023-07-21 01:46:50 +1000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,49 +12,48 @@ export const contractAddress = "0x228ecf90bA6a4085593F174Ae19Aa2dfE93BE763";
 function App() {
 
   const groupManagerContractAddress = "0xbE70957C20914625a60040b7075C900C6756939f";
+  const init = async () => {
+    const { ethereum } = window;
+    const provider = new ethers.BrowserProvider(ethereum);
+    const signer = await provider.getSigner();
+
+    // Initialize the GroupManager contract
+    const groupManagerContract = new ethers.Contract(groupManagerContractAddress, groupManagerContractABI, signer);
+    //setGroupManagerContract(groupManagerContract);
+    
+    groupManagerContract.on("db_createGroup", (...args) => {
+      alert(args);
+    });
+    groupManagerContract.on("db_updateStatus", (from, value, event) => {
+      console.log(from, value);
+      console.log(event.blockNumber);
+    });
+    groupManagerContract.on("db_updateInterestRate", (from, value, event) => {
+      console.log(from, value);
+      console.log(event.blockNumber);
+    });
+    groupManagerContract.on("db_updateMonthlyPayment", (from, value, event) => {
+      console.log(from, value);
+      console.log(event.blockNumber);
+    });
+    groupManagerContract.on("db_updatePeriod", (from, value, event) => {
+      console.log(from, value);
+      console.log(event.blockNumber);
+    });
+    return () => {
+      groupManagerContract.removeAllListeners("db_createGroup");
+      groupManagerContract.removeAllListeners("db_updateStatus");
+      groupManagerContract.removeAllListeners("db_updateInterestRate");
+      groupManagerContract.removeAllListeners("db_updateMonthlyPayment");
+      groupManagerContract.removeAllListeners("db_updatePeriod");
+    };
+    
+  }
 
   useEffect(() => {
-    const init = async () => {
-      const { ethereum } = window;
-      const provider = new ethers.BrowserProvider(ethereum);
-      const signer = await provider.getSigner();
-
-      // Initialize the GroupManager contract
-      const groupManagerContract = new ethers.Contract(groupManagerContractAddress, groupManagerContractABI, signer);
-      //setGroupManagerContract(groupManagerContract);
-      
-      groupManagerContract.on("db_createGroup", (from, value, event) => {
-        console.log(from, value);
-        alert(event);
-        console.log(event.blockNumber);
-      });
-      groupManagerContract.on("db_updateStatus", (from, value, event) => {
-        console.log(from, value);
-        console.log(event.blockNumber);
-      });
-      groupManagerContract.on("db_updateInterestRate", (from, value, event) => {
-        console.log(from, value);
-        console.log(event.blockNumber);
-      });
-      groupManagerContract.on("db_updateMonthlyPayment", (from, value, event) => {
-        console.log(from, value);
-        console.log(event.blockNumber);
-      });
-      groupManagerContract.on("db_updatePeriod", (from, value, event) => {
-        console.log(from, value);
-        console.log(event.blockNumber);
-      });
-      return () => {
-        groupManagerContract.removeAllListeners("db_createGroup");
-        groupManagerContract.removeAllListeners("db_updateStatus");
-        groupManagerContract.removeAllListeners("db_updateInterestRate");
-        groupManagerContract.removeAllListeners("db_updateMonthlyPayment");
-        groupManagerContract.removeAllListeners("db_updatePeriod");
-      };
-      
-    }
+    
     init();
-  }, [groupManagerContractAddress]);
+  }, []);
 
   return (
     <Router>
