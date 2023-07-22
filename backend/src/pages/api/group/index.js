b41ba42e353f63fc3f181a1a570c55b3180298de
DESKTOP-S3CAGQV\whati <whatifif@yahoo.com>
2023-07-22 23:33:25 +1000

    API (api/group) is added.

---
+++ backend/src/pages/api/group/index.js
@@ -0,0 +1,53 @@
+import { PrismaClient } from "@prisma/client";
+import Cors from 'cors';
+
+const prisma = new PrismaClient();
+const cors = Cors({
+  methods: ['GET', 'HEAD', 'POST'],
+});
+
+// Utility to run middleware manually in Next.js
+// This can be moved to a separate file
+export async function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+// GET /api/group
+export async function get(req, res) {
+  const users = await prisma.group.findMany();
+  res.json(users);
+}
+
+// POST /api/group
+export async function post(req, res) {
+  const result = await prisma.group.create({
+    data: {
+      ...req.body
+    },
+  });
+  res.json(result);
+}
+
+export default async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  const { method } = req;
+  
+  switch (method) {
+    case 'GET':
+      return get(req, res);
+    case 'POST':
+      return post(req, res);
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
