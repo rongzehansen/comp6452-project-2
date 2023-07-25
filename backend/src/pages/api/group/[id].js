962d76c64b0495abeb76488039fd61169b74eaf9
DESKTOP-S3CAGQV\whati <whatifif@yahoo.com>
2023-07-25 12:11:13 +1000

    group bug fixed

---
+++ backend/src/pages/api/group/[id].js
@@ -0,0 +1,77 @@
+import { PrismaClient } from "@prisma/client";
+import Cors from 'cors';
+
+const prisma = new PrismaClient();
+const cors = Cors({
+  methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
+});
+
+export async function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+// Handle GET requests with an ID
+export async function get(req, res) {
+    console.log('query', req.query)
+  const id = req.query.id;
+  if (id) {
+    const group = await prisma.group.findUnique({
+      where: { index: Number(id) },
+    });
+    res.json(group);
+  } else {
+    res.status(400).send('Bad Request: id is required');
+  }
+}
+
+// Handle PUT requests
+export async function put(req, res) {
+  const { index, ...updateData } = req.body; // Extract index from the request body
+  if (index) {
+    const updatedGroup = await prisma.group.update({
+      where: { index: Number(index) }, // Use index as the where clause
+      data: updateData, // Exclude index from the update data
+    });
+    res.json(updatedGroup);
+  } else {
+    res.status(400).send('Bad Request: index is required');
+  }
+}
+
+// Handle DELETE requests
+export async function del(req, res) {
+  const { index } = req.body;
+  if (index) {
+    const deletedGroup = await prisma.group.delete({
+      where: { index: Number(index) },
+    });
+    res.json(deletedGroup);
+  } else {
+    res.status(400).send('Bad Request: index is required');
+  }
+}
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
+
+  const { method } = req;
+  
+  switch (method) {
+    case 'GET':
+      return get(req, res);
+    case 'PUT':
+      return put(req, res);
+    case 'DELETE':
+      return del(req, res);
+    default:
+      res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
--- backend/src/pages/api/group/index.js
+++ backend/src/pages/api/group/index.js
@@ -17,20 +17,13 @@ export async function runMiddleware(req, res, fn) {
   });
 }
 
+// Handle GET requests without an ID
 export async function get(req, res) {
-  const id = req.params;
-  const index = id;
-  if (index) {
-    const group = await prisma.group.findUnique({
-      where: { index: Number(index) },
-    });
-    res.json(group);
-  } else {
-    const groups = await prisma.group.findMany();
-    res.json(groups);
-  }
+  const groups = await prisma.group.findMany();
+  res.json(groups);
 }
 
+// Handle POST requests
 export async function post(req, res) {
   const { index, name, owner } = req.body;
   if (index && name && owner) {
@@ -47,31 +40,6 @@ export async function post(req, res) {
   }
 }
 
-export async function put(req, res) {
-  const { index, ...updateData } = req.body; // Extract index from the request body
-  if (index) {
-    const updatedGroup = await prisma.group.update({
-      where: { index: Number(index) }, // Use index as the where clause
-      data: updateData, // Exclude index from the update data
-    });
-    res.json(updatedGroup);
-  } else {
-    res.status(400).send('Bad Request: index is required');
-  }
-}
-
-export async function del(req, res) {
-  const { index } = req.body;
-  if (index) {
-    const deletedGroup = await prisma.group.delete({
-      where: { index: Number(index) },
-    });
-    res.json(deletedGroup);
-  } else {
-    res.status(400).send('Bad Request: index is required');
-  }
-}
-
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
@@ -82,12 +50,8 @@ export default async function handler(req, res) {
       return get(req, res);
     case 'POST':
       return post(req, res);
-    case 'PUT':
-      return put(req, res);
-    case 'DELETE':
-      return del(req, res);
     default:
-      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+      res.setHeader('Allow', ['GET', 'POST']);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
